920e382e44928a2e7f6dfa21c640b2258d5026d8
Brenda  Sosa <brenda.sosa@gmail.com>
2022-07-01 15:27:14 -0400

    end of the game

--- game.js
+++ game.js
@@ -94,3 +94,25 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+//function to start a new game
+
+var startGame = function(){
+  for (var i = 0; i < enemyNames.length; i++){
+    if(playerHealth > 0){
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+
+    } else{
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+
+}
+
